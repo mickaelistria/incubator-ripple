1900cba3127977bbbda93aa08e9e4c9d5ea5422e
Ilya Buziuk <ibuziuk@exadel.com>
2014-08-13 19:27:00 +0300

    CORDOVASIM: Reimplementing LocalStorage fix for JavaFX 2.2 (JDK 7)

--- assets/cordovasim/javaFXLocalStorage.js
+++ assets/cordovasim/javaFXLocalStorage.js
@@ -5,12 +5,10 @@
  * JavaFX 2.2 (JDK 7) does not support localStorage - https://javafx-jira.kenai.com/browse/RT-29584
  * Fortunately fixed for JavaFX 8 (Lombard)
  *
- */
-if (!('localStorage' in window && window['localStorage'] !== null)) {
-    window.localStorage = {
-        setItem: function(key, vale) {
-            localStorage[key] = value;
-        },
-        clear: function() {}
-    }
-}
+ */ 
+window.customLocalStorage = window.localStorage || {
+    setItem: function(key, value) {
+        customLocalStorage[key] = value;
+    },
+    clear: function() {}
+};
